7e7b94e3e06c88226b2d7d5d08b9fb611cec072a
arif-bepari <2021331003@student.sust.edu>
2026-05-16 20:32:38 +0600

    Java 54-65

---
+++ Java Script Tutorials/Tut 54-65 Intro to JS/script.js
@@ -0,0 +1,27 @@
+
+
+// console.log("code id running")
+
+// var a =6
+
+// console.log("your number is " + a)
+
+// let obj = {
+//     name : "Arif",
+//     role : "Coder",
+//     company : "ai"
+// };
+// console.log(obj)
+
+// for (const key in obj) { 
+//     const element = obj[key];   
+//     console.log(key, element)
+// }
+
+let x =5;
+console.log(`I bow you ${x} taka you have to give me ${x}`);
+
+s ="sffd"
+console.log(s[0])
+s[0]="a"
+console.log(s[0], s)
